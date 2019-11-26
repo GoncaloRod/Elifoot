72cd9d50a1c608ebe6223cf2d431db15a77e5477
Gonçalo Rodrigues <rodriguesgoncalo604@gmail.com>
2019-11-26 15:00:27 +0000

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "Elifoot"
+    architecture "x86"
+    startproject "Elifoot"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Elifoot"
+    location "Elifoot"
+    kind "ConsoleApp"
+    language "C"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+    debugdir("bin/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.c"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src",
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
