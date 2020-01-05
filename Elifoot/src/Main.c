9a8d09bb676a9789ee1521e7a3f106c463fa8a22
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-01-05 17:28:16 +0000

    New stuff

--- Elifoot/src/Main.c
+++ Elifoot/src/Main.c
@@ -15,8 +15,9 @@ int main(int argc, char** argv)
 {
 	Team* teams;
 	Team* playerTeam;
-	Game* nextGame;
+	Game* nextGame, *tmpGame;
 	int teamsCount;
+	float expenses;
 
 	srand(time(NULL));
 
@@ -34,30 +35,67 @@ int main(int argc, char** argv)
 
 	nextGame = playerTeam->results.games;
 
-	// TODO: Game loop
 	for (int weekend = 0; weekend < (teamsCount - 1) * 2; weekend++, nextGame++)
 	{
 		ClrScr();
 
-		if (weekend % 4 == 0)
+		if ((weekend + 1) % 4 == 0 && playerTeam->funds > 0.0f)
 		{
-			// A month has passed
-			// TODO: Pay the bills
+			// A month has passed, pay the bills
+			expenses = 0;
+
+			expenses += playerTeam->stadiumExpenses;
+
+			for (int i = 0; i < playerTeam->squad.playersCount; i++)
+			{
+				expenses += playerTeam->squad.players[i].salary;
+			}
+
+			playerTeam->funds -= expenses;
+
+			if (playerTeam->funds <= 0)
+			{
+				printf("You went bankrupted! You will now lose every game until the end of the season!\n");
+
+				printf("\n");
+				WaitForKey();
+			}
 		}
 
-		printf("Next game: %s vs %s", teams[nextGame->hostsIndex].name, teams[nextGame->visitorsIndex].name);
+		// Only play game if team is not bankrupted
+		if (playerTeam->funds > 0)
+		{
+			printf("Next game: %s vs %s\n\n", teams[nextGame->hostsIndex].name, teams[nextGame->visitorsIndex].name);
 
-		PlayGame(teams, nextGame, playerTeam);
+			WaitForKey();
+
+			PlayGame(teams, nextGame, playerTeam);
+		}
+		else
+		{
+			nextGame->hostsGoals = teams + nextGame->hostsIndex == playerTeam ? 0 : 1;
+			nextGame->visitorsGoals = teams + nextGame->hostsIndex == playerTeam ? 1 : 0;
+		}
+		
+		// Simulate other teams games
+		ClrScr();
+
+		printf("Other teams results:\n\n");
 
 		for (int teamIndex = 0; teamIndex < teamsCount; teamIndex++)
 		{
 			if (teams[teamIndex].results.gamesPlayedCount < weekend + 1)
 			{
-				SimulateGame(teams, teams[teamIndex].results.games + weekend);
+				tmpGame = teams[teamIndex].results.games + weekend;
+
+				//SimulateGame(teams, tmpGame);
+
+				printf("%s %d - %d %s\n", teams[tmpGame->hostsIndex].name, tmpGame->hostsGoals, tmpGame->visitorsGoals, teams[tmpGame->visitorsIndex].name);
 			}
 		}
 
-		// TODO: Print results
+		printf("\n");
+		WaitForKey();
 	}
 
 	// Free dynamically allocated memory
