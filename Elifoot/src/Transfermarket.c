557ab9e17ee490b830943ee8af35445c2da60136
Gonçalo Rodrigues <rodriguesgoncalo604@gmail.com>
2020-01-07 20:52:00 +0000

    Done this

--- Elifoot/src/Transfermarket.c
+++ Elifoot/src/Transfermarket.c
@@ -1,96 +1,96 @@
 #include "Transfermarket.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "Utils.h"
 
 void HandleTransfermarket(Team* teams, int teamsCount, Team* playerTeam)
 {
-	printf("Funds: %f\n", playerTeam->funds);
 	Player player;
 
-	//variable that calculates the value of the transfer & variable that counts the amount of funds required to transfer the player 
+	// Variable that calculates the value of the transfer & variable that counts the amount of funds required to transfer the player 
 	float transferValue, fundsRequired;
 
-	//variables that receive the users picked team & picked player & count the amout of transfers done by the user
+	// Variables that receive the users picked team & picked player & count the amount of transfers done by the user
 	int userTeamPick, userPlayerPick, transferCount;
-	char userPick;
 	transferCount = 0;
 
 	ClrScr();
-	//the user picks a team to transfer a player from (easier to print info on console when reduced from 20000 choices to 20.)
+
+	printf("Funds: %.2f\n\n", playerTeam->funds);
+
+	// The user picks a team to transfer a player from (easier to print info on console when reduced from 20000 choices to 20.)
 	printf("Pick from which team you want to transfer the player from:\n");
 
-	//cycle that goes through all the teams 
+	// Cycle that goes through all the teams 
 	for (int i = 0; i < teamsCount; i++)
 	{
 		//prints the name of the teams on position i aka from 0 all the way to the number of teams
 		printf("[%d] %s\n", i + 1, teams[i].name);
 
 	}
+
 	scanf_s("%d", &userTeamPick);
 
-	if (strcmp(teams[userTeamPick - 1].name, playerTeam->name) == 0)
+	if (teams + userTeamPick - 1 == playerTeam)
 	{
 		ClrScr();
 		printf("You can't transfer from your own team!");
-		return HandleTransfermarket(teams, teamsCount, playerTeam);
+		return;
 	}
 	else
 	{
 		ClrScr();
-		//the user picks a player in order for the transfer value to be calculated
+		// The user picks a player in order for the transfer value to be calculated
 		printf("Pick the player you want to transfer to your team:\n");
 
-		//cycle that prints the players names filtering through the teams pick
-		for (int k = 0; k < teams[userTeamPick - 1].squad.playersCount; k++)
+		// Cycle that prints the players names filtering through the teams pick
+		for (int i = 0; i < teams[userTeamPick - 1].squad.playersCount; i++)
 		{
-			printf("[%d] %s\n", k + 1, teams[userTeamPick - 1].squad.players[k].name);
+			printf("[%d] %s\n", i + 1, teams[userTeamPick - 1].squad.players[i].name);
 		}
 
 		scanf_s("%d", &userPlayerPick);
 
+		ClrScr();
 
-	ClrScr();
+		transferValue = teams[userTeamPick - 1].squad.players[userPlayerPick - 1].salary * 5.0f * teams[userTeamPick - 1].squad.players[userPlayerPick - 1].contractYears;
 
+		// The amount of funds required is the amount of funds the team has minus the value of the transfer
+		fundsRequired = teams->funds - transferValue;
 
-	transferValue = teams[userTeamPick - 1].squad.players[userPlayerPick - 1].salary * 5.0f * teams[userTeamPick - 1].squad.players[userPlayerPick - 1].contractYears;
-
-	//the amount of funds required is the amount of funds the team has minus the value of the transfer
-	fundsRequired = teams->funds - transferValue;
-
-	if (teams->funds < transferValue)
-	{
-		printf("Insuficient funds\n");
-		printf("Current funds: %f\n", fundsRequired);
-	}
-	else
-	{
-		if (transferCount <= 3 && teams->squad.playersCount <= teams->squad.maxPlayers)
+		if (playerTeam->funds < transferValue)
 		{
-			for (int j = 0; j < teamsCount; j++)
-			{
-				if (strcmp(teams[userTeamPick - 1].squad.players[userPlayerPick - 1].name, teams[userTeamPick - 1].squad.players[j].name) == 0)
-				{
-					//adding the new player to the team - inserts the player picked by the user on the users squad
-					playerTeam->squad.playersCount[teams[userTeamPick - 1].squad.players[userPlayerPick - 1].name];
-
-					//setting the new salary for the player
-					teams[userTeamPick - 1].squad.players[userPlayerPick - 1].salary = teams[userTeamPick - 1].squad.players[userPlayerPick - 1].salary * 1.50F;
-
-					//keeps track of how many transfers the user has done and adds to the transfer count one transfer everytime this conditions passes 
-					transferCount++;
-					teamsCount++;
-
-					printf("Jogador adicionado com sucesso: %s\n", teams[userTeamPick - 1].squad.players[userPlayerPick - 1].name);
-				}
-			}
+			printf("Insufficient funds! You need more %f to buy this player.\n", playerTeam->funds);
 		}
 		else
 		{
-			printf("You have reached the limit of transfers.\n");
-			return;
+			if (transferCount <= 3 && teams->squad.playersCount < teams->squad.maxPlayers)
+			{
+				// Adding the new player to the team - inserts the player picked by the user on the users squad
+				memcpy(playerTeam->squad.players + playerTeam->squad.playersCount, teams[userTeamPick - 1].squad.players + userPlayerPick - 1, sizeof(Player));
+
+				// Disable old player
+				teams[userTeamPick - 1].squad.players[userPlayerPick - 1].enable = 0;
+
+				// Setting the new salary for the player
+				playerTeam->squad.players[playerTeam->squad.playersCount].salary = teams[userTeamPick - 1].squad.players[userPlayerPick - 1].salary * 1.50f;
+
+				// Keeps track of how many transfers the user has done and adds to the transfer count one transfer every time this conditions passes 
+				transferCount++;
+				playerTeam->squad.playersCount++;
+
+				printf("Your transfer was accepted! %s\n", teams[userTeamPick - 1].squad.players[userPlayerPick - 1].name);
+			}
+			else
+			{
+				printf("You have reached the limit of transfers.\n");
+				return;
+			}
 		}
+
+		WaitForKey();
 	}
-	WaitForKey();
-    }
 }
